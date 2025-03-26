17a678dae9550ce049d48a927294ed8d2647e080
Bao <shazhquan@gmail.com>
2025-03-26 17:46:22 +0800

    实现sort函数

--- main.cpp
+++ main.cpp
@@ -2,17 +2,29 @@
 using namespace std;
 
 void    sort    (int* arr, int size);
+{
+    int temp;
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }   
+
+}
 
 int main()
 {
     int arr[] = {5, 3, 2, 1, 4};
     int size = sizeof(arr) / sizeof(arr[0]);
 
-    sort(arr, size);
-
-    for (int i = 0; i < size; i++)
-        cout << arr[i] << " ";
-    cout << endl;
+    sort(arr, size);  
 
     return 0;
 }
