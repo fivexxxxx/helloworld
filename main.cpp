1e1c214616847359f68d2d20fd9c2ef384099894
Bao <shazhquan@gmail.com>
2025-03-26 21:49:56 +0800

    modify quicksort

--- main.cpp
+++ main.cpp
@@ -1,5 +1,37 @@
 #include <iostream>
 using namespace std;
+//修改算法为快速排序
+void quick_sort(int* arr, int low, int high)
+{
+    if (low >= high)
+    {
+        return;
+    }
+    int first = low;
+    int last = high;
+    int key = arr[first];    /*用字表的第一个记录作为枢轴*/
+
+    while (first < last)
+    {
+        while (first < last && arr[last] >= key)
+        {
+            --last;
+        }
+
+        arr[first] = arr[last];    /*将比第一个小的移到低端*/
+
+        while (first < last && arr[first] <= key)
+        {
+            ++first;
+        }
+        
+        arr[last] = arr[first];    /*将比第一个大的移到高端*/
+    }
+
+    arr[first] = key;    /*枢轴记录到位*/
+    quick_sort(arr, low, first - 1);
+    quick_sort(arr, first + 1, high);
+}
 
 void    sort    (int* arr, int size);
 {
